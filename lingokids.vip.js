46aa8049a70b9d0e92822ee4739d49376777853e
coolqoo <iharvard@qq.com>
2022-07-26 23:54:27 +0800

    Update lingokids.vip.js

--- lingokids.vip.js
+++ lingokids.vip.js
@@ -2,16 +2,17 @@ var obj = JSON.parse($response.body);
 
 obj["info"]["subscriptions"]= [
       {
-         "status":"trial",
-         "product":"unlimited",
-         "duration_unit":"months",
-         "id":3690701,
-         "in_grace_period":false,
-         "platform":"apple",
-         "starts_at":1658849749,
-         "duration_value":1,
-         "ends_at":4659454548,
-         "auto_renew_status":true,
+        "status": "trial",
+        "product": "unlimited",
+        "duration_unit": "months",
+        "id": 3690701,
+        "in_grace_period": false,
+        "platform": "apple",
+        "starts_at": 1658849749,
+        "duration_value": 1,
+        "ends_at": 1659454548,
+        "auto_renew_status": true,
+        "plan_id": "lk.ios.s1m.t7d.p15.v2"
       }];
 
 $done({body: JSON.stringify(obj)});
